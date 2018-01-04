844af5982a931e8fdce67cc7a7c8aeba39489ac8
Nicolas Sagnette <nicolas.sagnette@atinternet.com>
2018-01-04 07:18:45 +0100

    Input modification

--- flattrim.go
+++ flattrim.go
@@ -20,52 +20,48 @@ func NewFlattrimizer(caseType CaseType) *flattrimizer {
 	return &flattrimizer{caseType: caseType}
 }
 
-func (f *flattrimizer) Flatten(value interface{}) map[string]interface{} {
+func (f *flattrimizer) SetCaseType(caseType CaseType) {
+	f.caseType = caseType
+}
+
+func (f *flattrimizer) Flatten(value map[string]interface{}) map[string]interface{} {
 	return f.FlattenWithPrefix("", value)
 }
 
-func (f *flattrimizer) FlattenWithPrefix(prefix string, value interface{}) map[string]interface{} {
+func (f *flattrimizer) FlattenWithPrefix(prefix string, value map[string]interface{}) map[string]interface{} {
 	result := make(map[string]interface{})
 	f.flatten(result, prefix, value)
 	return result
 }
 
-func (f *flattrimizer) flatten(result map[string]interface{}, prefix string, value interface{}) {
+func (f *flattrimizer) flatten(result map[string]interface{}, prefix string, value map[string]interface{}) {
 
-	if data, ok := value.(map[string]interface{}); ok {
-		flattenKey := f.BuildFlattenKey(prefix)
+	flattenKey := f.BuildFlattenKey(prefix)
 
-		for key, val := range data {
+	for key, val := range value {
 
-			switch newVal := val.(type) {
-			case map[string]interface{}:
-				f.flatten(result, fmt.Sprint(flattenKey, key), newVal)
-			case []map[string]interface{}:
-				arrayObjects := make([]map[string]interface{}, 0)
+		switch newVal := val.(type) {
+		case map[string]interface{}:
+			f.flatten(result, fmt.Sprint(flattenKey, key), newVal)
+		case []map[string]interface{}:
+			arrayObjects := make([]map[string]interface{}, 0)
 
-				for _, obj := range newVal {
-					arrayObj := make(map[string]interface{})
-					f.flatten(arrayObj, "", obj)
-					arrayObjects = append(arrayObjects, arrayObj)
-				}
-				if f.caseType == LOWERCASE {
-					result[strings.ToLower(fmt.Sprint(flattenKey, key))] = arrayObjects
-				} else {
-					result[fmt.Sprint(flattenKey, key)] = arrayObjects
-				}
-			default:
-				if f.caseType == LOWERCASE {
-					result[strings.ToLower(fmt.Sprint(flattenKey, key))] = val
-				} else {
-					result[fmt.Sprint(flattenKey, key)] = val
-				}
+			for _, obj := range newVal {
+				arrayObj := make(map[string]interface{})
+				f.flatten(arrayObj, "", obj)
+				arrayObjects = append(arrayObjects, arrayObj)
 			}
-		}
-	} else {
-		if f.caseType == LOWERCASE {
-			result[strings.ToLower(prefix)] = data
-		} else {
-			result[prefix] = data
+			if f.caseType == LOWERCASE {
+				result[strings.ToLower(fmt.Sprint(flattenKey, key))] = arrayObjects
+			} else {
+				result[fmt.Sprint(flattenKey, key)] = arrayObjects
+			}
+		default:
+			if f.caseType == LOWERCASE {
+				result[strings.ToLower(fmt.Sprint(flattenKey, key))] = val
+			} else {
+				result[fmt.Sprint(flattenKey, key)] = val
+			}
 		}
 	}
 }
